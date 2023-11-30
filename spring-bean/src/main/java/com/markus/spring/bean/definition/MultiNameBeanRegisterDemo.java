3e9298a4964047ed93ff532288e5838eeaa514c4
markuszcl99 <e_markuszhang@163.com>
2023-11-30 18:52:36 +0800

    bean 标签 id 和 name的使用示例和说明

---
+++ spring-bean/src/main/java/com/markus/spring/bean/definition/MultiNameBeanRegisterDemo.java
@@ -0,0 +1,33 @@
+package com.markus.spring.bean.definition;
+
+import com.markus.spring.ioc.overview.domain.User;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.ListableBeanFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @Author: zhangchenglong06
+ * @Date: 2023/11/30
+ * @Description:
+ */
+public class MultiNameBeanRegisterDemo {
+  /**
+   * 当xml中一个Bean被指定为多个name且没有指定id的情况下，会默认指定name的第一个名称做为id，其余作为别名
+   * 当有指定id的情况，那么name中指定的名称将全部作为别名
+   */
+  public static void main(String[] args) {
+    ListableBeanFactory beanFactory = new ClassPathXmlApplicationContext("classpath:/META-INF/bean-definition.xml");
+    String[] beanDefinitionNames = beanFactory.getBeanDefinitionNames();
+    for (String beanDefinitionName : beanDefinitionNames) {
+      System.out.println(beanDefinitionName);
+    }
+
+    String[] aliases = beanFactory.getAliases("user");
+    for (String alias : aliases) {
+      System.out.println(alias);
+    }
+
+    User user2 = beanFactory.getBean("user2", User.class);
+    System.out.println(user2);
+  }
+}
