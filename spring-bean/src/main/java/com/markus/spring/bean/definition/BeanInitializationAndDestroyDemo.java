0956ed9557a8f027a2201dba3ad6e15986ebbdbf
markuszcl99 <e_markuszhang@163.com>
2023-12-05 12:58:08 +0800

    spring-bean 编写懒加载的示例代码

--- spring-bean/src/main/java/com/markus/spring/bean/definition/BeanInitializationAndDestroyDemo.java
+++ spring-bean/src/main/java/com/markus/spring/bean/definition/BeanInitializationAndDestroyDemo.java
@@ -5,6 +5,7 @@ import com.markus.spring.bean.factory.UserFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 
 /**
  * @Author: zhangchenglong06
@@ -18,12 +19,19 @@ public class BeanInitializationAndDestroyDemo {
     applicationContext.register(BeanInitializationAndDestroyDemo.class);
     // 启动应用上下文
     applicationContext.refresh();
+    System.out.println("应用上下文启动完成...");
     UserFactory bean = applicationContext.getBean(UserFactory.class);
     // 关闭应用上下文
+    System.out.println("应用上下文准备关闭...");
     applicationContext.close();
+    System.out.println("应用上下文关闭完成...");
   }
 
-  @Bean(initMethod = "initUserFactory",destroyMethod = "doDestroy")
+  /**
+   * // @Lazy 懒加载，只有在应用程序使用（依赖注入或者依赖查找）到时才会触发Bean的初始化
+   */
+  @Bean(initMethod = "initUserFactory", destroyMethod = "doDestroy")
+  @Lazy
   public DefaultUserFactory userFactory() {
     return new DefaultUserFactory();
   }
