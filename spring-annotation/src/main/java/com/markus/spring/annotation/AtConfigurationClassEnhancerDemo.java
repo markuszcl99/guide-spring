54bec675d950e039f14a61b9de50a923941eb259
markuszcl99 <e_markuszhang@163.com>
2023-12-10 00:36:23 +0800

    spring-annotation BeanFactoryAwareMethodInterceptor的作用备注

--- spring-annotation/src/main/java/com/markus/spring/annotation/AtConfigurationClassEnhancerDemo.java
+++ spring-annotation/src/main/java/com/markus/spring/annotation/AtConfigurationClassEnhancerDemo.java
@@ -1,10 +1,16 @@
 package com.markus.spring.annotation;
 
 import com.markus.spring.ioc.overview.domain.User;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.context.ResourceLoaderAware;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ConfigurationClassPostProcessor;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 
 /**
  * @author: markus
@@ -16,10 +22,25 @@ import org.springframework.context.annotation.ConfigurationClassPostProcessor;
  * // ConfigurationClassParser#doProcessConfigurationClass
  */
 @Configuration
-public class AtConfigurationClassEnhancerDemo {
+public class AtConfigurationClassEnhancerDemo implements BeanFactoryAware, ResourceLoaderAware {
+
+    private BeanFactory beanFactory;
+
+    private ResourceLoader resourceLoader;
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        this.beanFactory = beanFactory;
+    }
+
+    @Override
+    public void setResourceLoader(ResourceLoader resourceLoader) {
+        this.resourceLoader = resourceLoader;
+    }
 
     /**
      * 被代理原因: 保证 @Bean 方法得到正确的调用
+     * 代理类中 BeanFactoryAwareMethodInterceptor 的拦截作用是，为了在 拦截方法中使用 BeanFactory
      */
 
     public static void main(String[] args) {
