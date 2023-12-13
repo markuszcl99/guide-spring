43c48fc4f0f2f4c651322eafbbcc583351db7946
markuszcl99 <e_markuszhang@163.com>
2023-12-13 22:58:18 +0800

    spring-configuration-metadata 字符编码相关的知识点记录

--- spring-configuration-metadata/src/main/java/com/markus/spring/configuration/metadata/AnnotatedSpringAsIoCContainerDemo.java
+++ spring-configuration-metadata/src/main/java/com/markus/spring/configuration/metadata/AnnotatedSpringAsIoCContainerDemo.java
@@ -21,10 +21,30 @@ import java.util.Map;
     @ComponentScan(basePackages = "com.markus.spring.configuration.component")
 })
 @PropertySources({
+    // 不指定编码格式，框架会直接读取字节流，字节流在处理输入输出时，只会简单的读写字节
+    // 而指定了字符编码格式后，在处理输入输出时，是按照字符为单位进行读取，所以在一些情况下很有可能会出现乱码，比如出现中文
     @PropertySource(value = "classpath:/META-INF/user.properties", encoding = "UTF-8")
 })
 public class AnnotatedSpringAsIoCContainerDemo {
 
+  /**
+   * 字符编码 知识点
+   * 一个字符占用的字节数取决于具体的字符编码方式。以下是一些常见的字符编码方式及其对应的字节数：
+   * <p>
+   * ASCII：每个字符占用 1 个字节。ASCII 只能表示 128 个不同的字符，包括英文大小写字母、数字和一些特殊字符。
+   * <p>
+   * ISO-8859-1：每个字符占用 1 个字节。ISO-8859-1 可以表示 256 个不同的字符，包括西欧语言的字符。
+   * <p>
+   * GBK：每个字符占用 1~2 个字节。GBK 是一种用于表示简体中文字符的编码方式，它可以表示 21003 个不同的字符。
+   * <p>
+   * UTF-8：每个字符占用 1~4 个字节。UTF-8 是一种用于表示 Unicode 字符的编码方式，它可以表示所有的 Unicode 字符。
+   * <p>
+   * UTF-16：每个字符占用 2 或 4 个字节。UTF-16 也是一种用于表示 Unicode 字符的编码方式。
+   * <p>
+   * UTF-32：每个字符占用 4 个字节。UTF-32 也是一种用于表示 Unicode 字符的编码方式。
+   * <p>
+   * 注意，以上的字节数是指一个字符在内存中或在文件中的存储大小，而不是在显示时的宽度。例如，一个中文字符在显示时通常占用两个英文字符的宽度，但在 UTF-8 编码下，它可能占用 3 个字节的存储空间。
+   */
   @Bean("user-by-property")
   public User user(@Value("${user.id}") Long userId,
                    @Value("${user.name}") String userName,
