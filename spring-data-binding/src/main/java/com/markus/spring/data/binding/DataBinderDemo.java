7b83bd851f873b67427beebd3f7ccdaf6f0f01ab
markuszcl99 <e_markuszhang@163.com>
2023-12-27 13:21:38 +0800

    spring-data-binding DataBinder 更改绑定规则示例+绑定结果BindingResult和国际化文案MessageSource结合案例

--- spring-data-binding/src/main/java/com/markus/spring/data/binding/DataBinderDemo.java
+++ spring-data-binding/src/main/java/com/markus/spring/data/binding/DataBinderDemo.java
@@ -1,13 +1,17 @@
 package com.markus.spring.data.binding;
 
-import com.markus.spring.ioc.overview.domain.Company;
 import com.markus.spring.ioc.overview.domain.User;
 import org.springframework.beans.BeanWrapper;
 import org.springframework.beans.MutablePropertyValues;
+import org.springframework.context.MessageSource;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.validation.BeanPropertyBindingResult;
+import org.springframework.validation.BindingResult;
 import org.springframework.validation.DataBinder;
+import org.springframework.validation.FieldError;
 
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 
 /**
@@ -24,15 +28,15 @@ public class DataBinderDemo {
 
     Map<String, Object> source = new HashMap<>();
     source.put("id", "1");
-    source.put("username", "markus zhang");
+//    source.put("username", "markus zhang");
 
     // 1. PropertyValues 中包含 User 中没有的配置，看下 数据绑定期间有什么问题
     source.put("myName", "zhangcl");
 
     // 2. PropertyValues 中包含嵌套类型
-    Company company = new Company();
-    company.setCompanyName("MT");
-    source.put("company", company);
+//    Company company = new Company();
+//    company.setCompanyName("MT");
+//    source.put("company", company);
     source.put("company.companyName", "MT");
 
     // 3. PropertyValues 包含枚举类型
@@ -42,13 +46,36 @@ public class DataBinderDemo {
 //    dataBinder.setIgnoreUnknownFields(false);
 
     // 调整 自动生成嵌套属性
-    dataBinder.setAutoGrowNestedPaths(false);
-    dataBinder.setIgnoreInvalidFields(true);
+//    dataBinder.setAutoGrowNestedPaths(false);
+//    dataBinder.setIgnoreInvalidFields(true);
+
+    // 设置允许绑定的字段，包括"id"、"username"和"city"字段。
+//    dataBinder.setAllowedFields("id", "username", "city");
+    // 设置不允许绑定的字段
+//    dataBinder.setDisallowedFields("company");
+    // 设置必须要绑定的字段，这里不会报错，会将绑定结果记录到BindingResult中
+    dataBinder.setRequiredFields("id", "username");
+
 
     MutablePropertyValues propertyValues = new MutablePropertyValues(source);
-
     dataBinder.bind(propertyValues);
 
     System.out.println(user);
+    BindingResult bindingResult = dataBinder.getBindingResult();
+    System.out.println(bindingResult);
+    MessageSource messageSource = createMessageSource();
+    for (FieldError fieldError : bindingResult.getFieldErrors()) {
+      String code = "required." + fieldError.getField();
+      String message = messageSource.getMessage(code, fieldError.getArguments(), fieldError.getDefaultMessage(), Locale.getDefault());
+      System.out.println(message);
+    }
+  }
+
+
+  static MessageSource createMessageSource() {
+    ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+    messageSource.addBasenames("META-INF/error");
+    messageSource.setDefaultEncoding("UTF-8");
+    return messageSource;
   }
 }
