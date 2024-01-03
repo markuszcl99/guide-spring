edc21cb4c402ae7bfe755289f80989bfec3d6905
markuszcl99 <e_markuszhang@163.com>
2024-01-03 13:07:50 +0800

    spring-generic Java 泛型API 示例 对Java 5提出的Type相关类进行示例练习

--- spring-generic/src/main/java/com/markus/spring/generic/GenericAPIDemo.java
+++ spring-generic/src/main/java/com/markus/spring/generic/GenericAPIDemo.java
@@ -2,7 +2,9 @@ package com.markus.spring.generic;
 
 import java.lang.reflect.*;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Stream;
 
 /**
@@ -21,38 +23,60 @@ public class GenericAPIDemo {
   List<String>[] lists;
   List<? extends Number> numbers;
 
+  Map.Entry<String, String> mapEntry;
+
   public static void main(String[] args) throws NoSuchFieldException {
     // 1. raw types : 原生类型 eg. List<Integer> 中的 List 就是原生类型，简而言之就是没有泛型的非数组、原始类型都是原生类型
     Class<?> listClass = ArrayList.class;
-    System.out.println(listClass);
+    System.out.println("raw types : " + listClass);
 
     // 2. array types : 数组类型
     Class<?> arrayClass = int[].class;
-    System.out.println(arrayClass);
+    System.out.println("array types : " + arrayClass);
 
     // 3. parameterized types : 泛型参数类型
     ParameterizedType parameterizedType = (ParameterizedType) ArrayList.class.getGenericSuperclass();
-    System.out.println(parameterizedType);
+    System.out.println("parameterized types : " + parameterizedType);
 
-    // 4 generic array types : 泛型数组类型
-    Field field = GenericAPIDemo.class.getDeclaredField("lists");
-    Type genericType = field.getGenericType();
-    System.out.println(genericType);
-
-    // 5. type variable : 泛型类型变量
+    // 4. type variable : 泛型类型变量
     Type[] actualTypeArguments = parameterizedType.getActualTypeArguments();
-
+    System.out.print("AbstractList type arguments : ");
     Stream.of(actualTypeArguments)
         .map(TypeVariable.class::cast) // Type --> TypeVariable
         .forEach(System.out::println);
+
+    parameterizedType = (ParameterizedType) HashMap.class.getGenericSuperclass();
+    actualTypeArguments = parameterizedType.getActualTypeArguments();
+    System.out.print("Map type arguments : ");
+    Stream.of(actualTypeArguments)
+        .map(TypeVariable.class::cast) // Type --> TypeVariable
+        .forEach(typeVariable -> System.out.print(typeVariable + " "));
+
+    // 5. generic array types : 泛型数组类型
+    Field field = GenericAPIDemo.class.getDeclaredField("lists");
+    GenericArrayType genericType = (GenericArrayType) field.getGenericType();
+    System.out.printf("\ngeneric array type : %s \n", genericType);
+    parameterizedType = (ParameterizedType) genericType.getGenericComponentType();
+    System.out.println("ParameterizedType.getRawType : " + parameterizedType.getRawType());
+    System.out.println("ParameterizedType.getOwnerType : " + parameterizedType.getOwnerType());
+    actualTypeArguments = parameterizedType.getActualTypeArguments();
+    System.out.print("concrete type arguments : ");
+    Stream.of(actualTypeArguments)
+        .map(Class.class::cast) // 参数类型具体化
+        .forEach(clazz -> System.out.print(clazz + " "));
+
+    field = GenericAPIDemo.class.getDeclaredField("mapEntry");
+    parameterizedType = (ParameterizedType) field.getGenericType();
+    System.out.println("\nMap.Entry<String, String> ownerType : " + parameterizedType.getOwnerType());
+
 
     // 6. wildcard types : 泛型通配符类型
     field = GenericAPIDemo.class.getDeclaredField("numbers");
     parameterizedType = (ParameterizedType) field.getGenericType();
     WildcardType wildcardType = (WildcardType) parameterizedType.getActualTypeArguments()[0];
     // ? extends Number
-    System.out.println(wildcardType);
+    System.out.println("WildcardType : " + wildcardType);
     // Number
-    System.out.println(wildcardType.getUpperBounds()[0]);
+    System.out.println("WildcardType's UpperBounds : " + wildcardType.getUpperBounds()[0]);
   }
 }
