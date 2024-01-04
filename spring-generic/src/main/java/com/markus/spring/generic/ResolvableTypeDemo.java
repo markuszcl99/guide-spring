805fc9e751a7c8e38fc79cfcce8ef5ed5e89bbb6
markuszcl99 <e_markuszhang@163.com>
2024-01-04 23:33:06 +0800

    spring-generic 验证下泛型擦除的结论：当使用泛型 List<T> 时 会被擦除为 原生类型 List，使用泛型 T 时，该对象会被擦除为 Object。当指定泛型参数边界时，即 List<T extends String> 、T extends String 则会保留参数类型为 泛型边界类型

--- spring-generic/src/main/java/com/markus/spring/generic/ResolvableTypeDemo.java
+++ spring-generic/src/main/java/com/markus/spring/generic/ResolvableTypeDemo.java
@@ -2,6 +2,7 @@ package com.markus.spring.generic;
 
 import org.springframework.core.ResolvableType;
 
+import java.lang.reflect.Method;
 import java.util.List;
 import java.util.Map;
 
@@ -13,39 +14,72 @@ import java.util.Map;
  */
 public class ResolvableTypeDemo {
 
-  Map<String, List<Integer>> map;
+    Map<String, List<Integer>> map;
 
-  public static void main(String[] args) throws NoSuchFieldException {
-    ResolvableType resolvableType = ResolvableType.forClass(StringList.class);
-    System.out.printf("ResolvableType.forClass(%s) : %s\n", StringList.class, resolvableType);
-    ResolvableType generic = resolvableType.getGeneric(1);
-    System.out.println(generic);
-    ResolvableType superType = resolvableType.getSuperType();
-    System.out.println(superType);
-    Class<?> rawClass = superType.getRawClass();
-    System.out.println(rawClass);
-    generic = superType.getGeneric(0);
-    System.out.println(generic.getRawClass().cast("123"));
+    Map<? extends String, ? extends Integer> wildcardMap;
 
-    Class<?> resolve = resolvableType.getSuperType().getGeneric(0).resolve();
-    System.out.println(resolve);
+    public static void main(String[] args) throws NoSuchFieldException, NoSuchMethodException {
+        ResolvableType resolvableType = ResolvableType.forClass(StringList.class);
+        System.out.printf("ResolvableType.forClass(%s) : %s\n", StringList.class, resolvableType);
+        ResolvableType generic = resolvableType.getGeneric(1);
+        System.out.println(generic);
+        ResolvableType superType = resolvableType.getSuperType();
+        System.out.println(superType);
+        Class<?> rawClass = superType.getRawClass();
+        System.out.println(rawClass);
+        generic = superType.getGeneric(0);
+        System.out.println(generic.getRawClass().cast("123"));
 
-    resolvableType = ResolvableType.forField(ResolvableTypeDemo.class.getDeclaredField("map"));
-    resolve = resolvableType.resolve();
-    System.out.println(resolve);
+        Class<?> resolve = resolvableType.getSuperType().getGeneric(0).resolve();
+        System.out.println(resolve);
 
-    resolve = resolvableType.getGeneric(0).resolve();
-    System.out.println(resolve);
+        resolvableType = ResolvableType.forField(ResolvableTypeDemo.class.getDeclaredField("map"));
+        resolve = resolvableType.resolve();
+        System.out.println(resolve);
 
-    resolve = resolvableType.getGeneric(1).resolve();
-    System.out.println(resolve);
+        resolve = resolvableType.getGeneric(0).resolve();
+        System.out.println(resolve);
 
-    resolve = resolvableType.getGeneric(1, 0).resolve();
-    System.out.println(resolve);
+        resolve = resolvableType.getGeneric(1).resolve();
+        System.out.println(resolve);
 
-  }
+        resolve = resolvableType.getGeneric(1, 0).resolve();
+        System.out.println(resolve);
 
-//  public <T> T getObject() {
-//
-//  }
+        resolvableType = ResolvableType.forField(ResolvableTypeDemo.class.getDeclaredField("wildcardMap"));
+        resolve = resolvableType.resolve();
+        System.out.println("wildcardMap resolvableType : " + resolve);
+        resolve = resolvableType.asMap().resolve();
+        System.out.println("wildcardMap resolvableType.asMap() : " + resolve);
+        resolve = resolvableType.getGeneric(0).resolve();
+        System.out.println("wildcardMap resolvableType.getGeneric(0) : " + resolve);
+        resolve = resolvableType.getGeneric(1).resolve();
+        System.out.println("wildcardMap resolvableType.getGeneric(1) : " + resolve);
+
+        Method method = ResolvableTypeDemo.class.getDeclaredMethod("getObject");
+        resolvableType = ResolvableType.forMethodReturnType(method);
+        System.out.println("泛型方法未指定泛型参数边界 : " + resolvableType.resolve());
+
+        method = ResolvableTypeDemo.class.getDeclaredMethod("getGenericBoundObject");
+        resolvableType = ResolvableType.forMethodReturnType(method);
+        System.out.println("泛型方法指定泛型参数边界 : " + resolvableType.resolve());
+
+        method = ResolvableTypeDemo.class.getDeclaredMethod("getConcreteObject");
+        resolvableType = ResolvableType.forMethodReturnType(method);
+        System.out.println("具体方法 : " + resolvableType.resolve());
+
+    }
+
+    public <T> T getObject() {
+        return null;
+    }
+
+    public <T extends String> T getGenericBoundObject() {
+        return null;
+    }
+
+    public String getConcreteObject() {
+        return null;
+    }
+
 }
