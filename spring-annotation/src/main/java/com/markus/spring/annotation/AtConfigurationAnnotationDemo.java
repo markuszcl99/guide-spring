758e2accc4dd9a1af7e409dc9cd398d177623a07
markuszcl99 <e_markuszhang@163.com>
2024-01-10 09:49:09 +0800

    spring-annotation 提交下 @Configuration 会做字节码提升的示例

--- spring-annotation/src/main/java/com/markus/spring/annotation/AtConfigurationAnnotationDemo.java
+++ spring-annotation/src/main/java/com/markus/spring/annotation/AtConfigurationAnnotationDemo.java
@@ -1,6 +1,7 @@
 package com.markus.spring.annotation;
 
 import com.markus.spring.annotation.config.BeanConfig;
+import com.markus.spring.annotation.config.BeanConfig2;
 import com.markus.spring.ioc.overview.domain.User;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -24,6 +25,11 @@ public class AtConfigurationAnnotationDemo {
       System.out.println(key + ": " + value);
     });
     System.out.println("==============================");
+
+    BeanConfig2 beanConfig2 = applicationContext.getBean(BeanConfig2.class);
+    User manualInvoke = beanConfig2.user2();
+    User lookup = applicationContext.getBean("user2", User.class);
+    System.out.println("manualInvoke == lookup : " + (manualInvoke == lookup));
     applicationContext.close();
   }
 }
--- spring-annotation/src/main/java/com/markus/spring/annotation/config/BeanConfig2.java
+++ spring-annotation/src/main/java/com/markus/spring/annotation/config/BeanConfig2.java
@@ -2,12 +2,14 @@ package com.markus.spring.annotation.config;
 
 import com.markus.spring.ioc.overview.domain.User;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * @Author: zhangchenglong06
  * @Date: 2024/1/9
  * @Description:
  */
+@Configuration // 字节码提升 生成代理，防止当前 配置类里面的 @Bean 方法被调用时产生新的实例
 public class BeanConfig2 {
   @Bean
   public User user2() {
