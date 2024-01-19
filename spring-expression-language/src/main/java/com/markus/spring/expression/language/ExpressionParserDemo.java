fc57abc700fd191197036119c9b32594f183d351
markuszcl99 <e_markuszhang@163.com>
2024-01-19 13:26:39 +0800

    spring-expression-language SpEL 方法调用特性的示例

--- spring-expression-language/src/main/java/com/markus/spring/expression/language/ExpressionParserDemo.java
+++ spring-expression-language/src/main/java/com/markus/spring/expression/language/ExpressionParserDemo.java
@@ -13,7 +13,18 @@ import org.springframework.expression.spel.standard.SpelExpressionParser;
 public class ExpressionParserDemo {
   public static void main(String[] args) {
     ExpressionParser parser = new SpelExpressionParser();
-    Expression exp = parser.parseExpression("'Hello,World!'");
+    // 特性一 : 计算 字面量
+    Expression exp = parser.parseExpression("'Hello,World'");
+    System.out.println(exp.getValue());
+
+    // 特性二 : 方法调用
+    exp = parser.parseExpression("'Hello,World'.concat('!')");
+    System.out.println(exp.getValue());
+    //         调用 getBytes()
+    exp = parser.parseExpression("'Hello,World'.bytes");
+    System.out.println(exp.getValue());
+    //         嵌套调用 getBytes().length
+    exp = parser.parseExpression("'Hello,World'.bytes.length");
     System.out.println(exp.getValue());
   }
 }
