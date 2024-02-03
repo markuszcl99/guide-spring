f9927060aad70dd5ab9bc54fb443be476df614ff
markuszcl99 <e_markuszhang@163.com>
2024-02-03 16:23:56 +0800

    spring-transaction 整理下 代码格式

--- spring-transaction/src/main/java/com/markus/spring/transaction/service/UserService.java
+++ spring-transaction/src/main/java/com/markus/spring/transaction/service/UserService.java
@@ -4,6 +4,7 @@ import com.markus.spring.data.jdbc.domain.entity.User;
 import com.markus.spring.data.jdbc.repository.UserDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Objects;
@@ -16,34 +17,34 @@ import java.util.Objects;
 @Service
 public class UserService {
 
-  @Autowired
-  private UserDao userDao;
+    @Autowired
+    private UserDao userDao;
 
-  //  @Transactional
-  public void processUser() {
-    User user = new User();
-    user.setName("markus zhang unique time :" + System.currentTimeMillis());
-    user.setAge(25);
-    user.setAddress("山东菏泽");
+    @Transactional
+    public void processUser() {
+        User user = new User();
+        user.setName("markus zhang unique time :" + System.currentTimeMillis());
+        user.setAge(25);
+        user.setAddress("山东菏泽");
 
-    // 1. 先向数据库中插入一条数据
-    userDao.insertUser(user);
+        // 1. 先向数据库中插入一条数据
+        userDao.insertUser(user);
 
-    // 故意抛出一个异常，验证下 第一步 的操作是否会回滚
-    int i = 1 / 0;
+        // 故意抛出一个异常，验证下 第一步 的操作是否会回滚
+        int i = 1 / 0;
 
-    // 2. 再查询该数据
-    User queryUserByName = userDao.queryUserByName(user.getName());
-    if (Objects.isNull(queryUserByName)) {
-      return;
+        // 2. 再查询该数据
+        User queryUserByName = userDao.queryUserByName(user.getName());
+        if (Objects.isNull(queryUserByName)) {
+            return;
+        }
+        // 3. 再更新该数据到数据库中
+        queryUserByName.setAddress("北京朝阳");
+        userDao.updateUser(queryUserByName);
     }
-    // 3. 再更新该数据到数据库中
-    queryUserByName.setAddress("北京朝阳");
-    userDao.updateUser(queryUserByName);
-  }
 
-  public List<User> queryAllUsers() {
-    List<User> users = userDao.queryUsers(-1);
-    return users;
-  }
+    public List<User> queryAllUsers() {
+        List<User> users = userDao.queryUsers(-1);
+        return users;
+    }
 }
