147b2bf445b2776a4f776506294dd253f9fdf44a
markuszcl99 <e_markuszhang@163.com>
2024-02-09 08:56:48 +0800

    spring-cache 接入 redis 示例

--- spring-cache/src/main/java/com/markus/spring/cache/UseCacheDemo.java
+++ spring-cache/src/main/java/com/markus/spring/cache/UseCacheDemo.java
@@ -1,17 +1,23 @@
 package com.markus.spring.cache;
 
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.data.redis.core.RedisTemplate;
 
 /**
  * @Author: zhangchenglong06
  * @Date: 2024/2/1
  * @Description: 使用缓存示例
  */
-@EnableCaching
 public class UseCacheDemo {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:/META-INF/spring-data-redis-context.xml");
 
-  }
+        RedisTemplate redisTemplate = context.getBean(RedisTemplate.class);
+        redisTemplate.opsForValue().set("my_name", "markuszhang");
+        String myName = (String) redisTemplate.opsForValue().get("my_name");
+        System.out.println(myName);
+    }
 
 }
