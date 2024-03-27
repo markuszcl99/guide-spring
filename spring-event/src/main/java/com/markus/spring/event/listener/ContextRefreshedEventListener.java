58e6666e346388bd7710e3951cd6b2aa40b0c2e7
markuszcl99 <e_markuszhang@163.com>
2024-03-27 23:16:15 +0800

    spring-event 事件相关的示例代码

--- spring-event/src/main/java/com/markus/spring/event/listener/ContextRefreshedEventListener.java
+++ spring-event/src/main/java/com/markus/spring/event/listener/ContextRefreshedEventListener.java
@@ -1,5 +1,8 @@
 package com.markus.spring.event.listener;
 
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 
@@ -10,8 +13,14 @@ import org.springframework.context.event.ContextRefreshedEvent;
  */
 public class ContextRefreshedEventListener implements ApplicationListener<ContextRefreshedEvent> {
 
-  @Override
-  public void onApplicationEvent(ContextRefreshedEvent event) {
-    System.out.println(event);
-  }
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+        ApplicationContext context = (ApplicationContext) event.getSource();
+        String[] beanDefinitionNames = context.getBeanDefinitionNames();
+        System.out.println("=============== 开始打印 Bean Name ===============");
+        for (String beanDefinitionName : beanDefinitionNames) {
+            System.out.println(beanDefinitionName);
+        }
+        System.out.println("=============== 结束打印 Bean Name ===============");
+    }
 }
